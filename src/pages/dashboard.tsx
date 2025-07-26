b8a3f37a2f0500d5ec8051374fe632ef036f0459
recursionramu <maisamarthhoon@gmail.com>
2025-07-26 22:39:27 +0530

    commit

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,22 @@
-import sidebar from "@/Components/sidebar";
 
+import Sidebar, { SidebarBody, SidebarLink } from "../Components/sidebar";
+import { IconHome, IconUser } from "@tabler/icons-react"; // Example icons
 
+const links = [
+  { label: "Home", href: "/", icon: <IconHome /> },
+  { label: "Profile", href: "/profile", icon: <IconUser /> },
+    
+];
 
+export default function MyPage() {
+  return (
+    <Sidebar>
+      <SidebarBody>
+        {links.map((link) => (
+          <SidebarLink key={link.href} link={link} />
+        ))}
+      </SidebarBody>
+    
+    </Sidebar>
+  );
+}
